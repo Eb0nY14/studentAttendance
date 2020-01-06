964d18ad48eb1c920dd2f6776d2d473daa3a22f0
Christiana Temiola <c00220551@itcarlow.ie>
2020-01-06 11:56:01 +0000

    Updated Student.java file

--- Student.java
+++ Student.java
@@ -13,7 +13,7 @@ public class Student {
         if (attendance < 50) return AttendanceGrade.POOR;
         if (attendance < 70) return AttendanceGrade.AVERAGE;
         if (attendance <= 90) return AttendanceGrade.GOOD;
-        return AttendanceGrade.VE_GOOD;
+        return AttendanceGrade.VERY_GOOD;
         
     }
 }
