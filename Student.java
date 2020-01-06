6261c74104f2b5237df231a9915e0b1e28588a5d
Christiana Temiola <c00220551@itcarlow.ie>
2020-01-06 11:38:48 +0000

    Updated Student file

--- Student.java
+++ Student.java
@@ -9,12 +9,11 @@ public class Student {
 
     public AttendanceGrade getAttendanceGrade(int attendance) {
         if (attendance < 0 || attendance > 100) throw new IllegalArgumentException();
-        if (attendance >= 90) return AttendanceGrade.VERY_GOOD;
-        else if (attendance >= 70 && attendance < 90) return AttendanceGrade.GOOD;
-        else if (attendance >= 50 && attendance < 70) return AttendanceGrade.AVERAGE;
-        else if (attendance >= 30 && attendance < 50) return AttendanceGrade.POOR;	
-        else if (attendance > 0 && attendance < 30) return AttendanceGrade.VERY_POOR;
-        else return AttendanceGrade.ABSENT;
+        if (attendance < 30) return AttendanceGrade.VERY_POOR;
+        if (attendance < 50) return AttendanceGrade.POOR;
+        if (attendance < 70) return AttendanceGrade.AVERAGE;
+        if (attendance <= 90) return AttendanceGrade.GOOD;
+        return AttendanceGrade.VER_GOOD;
         
     }
 }
